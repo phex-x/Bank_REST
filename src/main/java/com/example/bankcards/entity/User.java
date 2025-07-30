1aed934370899609791ac86b13e9fc209417769d
Dmitriy Serov <dmitriiserov@MacBook-Air-Dmitriy.local>
2025-07-30 09:13:49 +0300

    created model for user

---
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.bankcards.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "owner")
+    List<Card> cards = new ArrayList<>();
+}
