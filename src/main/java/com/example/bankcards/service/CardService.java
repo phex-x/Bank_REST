da5571dc565635e188275489e4b9968a6f29fc95
Dmitriy Serov <dmitriiserov@192.168.1.3>
2025-08-01 14:23:15 +0300

    added method for changing card status

--- src/main/java/com/example/bankcards/service/CardService.java
+++ src/main/java/com/example/bankcards/service/CardService.java
@@ -3,13 +3,13 @@ package com.example.bankcards.service;
 import com.example.bankcards.dto.CardCreateDTO;
 import com.example.bankcards.dto.CardResponseDTO;
 import com.example.bankcards.entity.Card;
+import com.example.bankcards.entity.CardStatus;
 import com.example.bankcards.exception.CardNotFoundException;
 import com.example.bankcards.repository.CardRepository;
 import com.example.bankcards.util.mapper.CardMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 
 @Service
 @Slf4j
@@ -30,11 +30,24 @@ public class CardService {
     public CardResponseDTO getCardById(Long id) {
         Card card = cardRepository.findById(id)
                 .orElseThrow(() -> new CardNotFoundException("card with id: " + id + " not found"));
-
+        log.info("Card found: {}", card);
         return cardMapper.toDTO(card);
     }
 
     public void deleteCard(Long id) {
-        cardRepository.deleteById(id);
+        Card card = cardRepository.findById(id)
+                        .orElseThrow(() -> new CardNotFoundException("card with id: " + id + " not found"));
+        cardRepository.delete(card);
+        log.info("Card deleted: {}", id);
+    }
+
+    public CardResponseDTO changeStatus(Long id, CardStatus status) {
+        Card card = cardRepository.findById(id)
+                .orElseThrow(() -> new CardNotFoundException("card with id: " + id + " not found"));
+        card.setStatus(status);
+        Card savedCard = cardRepository.save(card);
+        log.info("Card updated: {}", savedCard);
+
+        return cardMapper.toDTO(savedCard);
     }
 }
