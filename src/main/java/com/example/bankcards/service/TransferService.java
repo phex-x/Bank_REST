224183a994bfcf295a6930e8f877b6b3353261b9
Dmitriy Serov <dmitriiserov@192.168.1.3>
2025-08-01 14:23:52 +0300

    created service

---
+++ src/main/java/com/example/bankcards/service/TransferService.java
@@ -0,0 +1,44 @@
+package com.example.bankcards.service;
+
+import com.example.bankcards.dto.TransferRequestDTO;
+import com.example.bankcards.dto.TransferResponseDTO;
+import com.example.bankcards.entity.Card;
+import com.example.bankcards.entity.Transfer;
+import com.example.bankcards.exception.CardNotFoundException;
+import com.example.bankcards.exception.InsufficientFundsException;
+import com.example.bankcards.exception.NotOwnedCardTransferException;
+import com.example.bankcards.repository.CardRepository;
+import com.example.bankcards.repository.TransferRepository;
+import com.example.bankcards.util.mapper.TransferMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class TransferService {
+    private final TransferRepository transferRepository;
+    private final CardRepository cardRepository;
+    private final TransferMapper transferMapper;
+
+    public TransferResponseDTO createTransfer(TransferRequestDTO transferRequestDTO) {
+        Card sourceCard = cardRepository.findById(transferRequestDTO.getSourceCardId())
+                .orElseThrow(() -> new CardNotFoundException("card with id " + transferRequestDTO.getSourceCardId() + " not found"));
+        Card destinationCard = cardRepository.findById(transferRequestDTO.getDestinationCardId())
+                .orElseThrow(() -> new CardNotFoundException("card with id " + transferRequestDTO.getDestinationCardId() + " not found"));
+
+        if (sourceCard.getBalance().compareTo(transferRequestDTO.getAmount()) < 0) {
+            throw new InsufficientFundsException("insufficient funds on source card");
+        }
+
+        if (!sourceCard.getOwner().equals(destinationCard.getOwner())) {
+            throw new NotOwnedCardTransferException("attempt to transfer not between your cards");
+        }
+        Transfer transfer = transferMapper.toEntity(transferRequestDTO);
+        transferRepository.save(transfer);
+        log.info("Transfer created");
+
+        return transferMapper.toDTO(transfer);
+    }
+}
