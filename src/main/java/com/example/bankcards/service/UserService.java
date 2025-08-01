80fd6509b0bc44302ad9d5d8c0fcfbce1d1559bb
Dmitriy Serov <dmitriiserov@192.168.1.3>
2025-08-01 12:09:45 +0300

    add crate, delete, get by id, change rol methods for user

---
+++ src/main/java/com/example/bankcards/service/UserService.java
@@ -0,0 +1,49 @@
+package com.example.bankcards.service;
+
+import com.example.bankcards.dto.UserCreateDTO;
+import com.example.bankcards.dto.UserResponseDTO;
+import com.example.bankcards.entity.Role;
+import com.example.bankcards.entity.User;
+import com.example.bankcards.exception.UserNotFoundException;
+import com.example.bankcards.repository.UserRepository;
+import com.example.bankcards.util.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserResponseDTO createUser(UserCreateDTO userCreateDTO) {
+        User user = userMapper.toEntity(userCreateDTO);
+        user = userRepository.save(user);
+        log.info("Created user: {}", user);
+
+        return userMapper.toDTO(user);
+    }
+
+    public UserResponseDTO getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("user with id + " + id + " not found"));
+
+        return userMapper.toDTO(user);
+    }
+
+    public void deleteUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("user with id + " + id + " not found"));
+        userRepository.delete(user);
+        log.info("Deleted user: {}", id);
+    }
+
+    public void changeRole(Long userId, Role role) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("user with id + " + userId + " not found"));
+        user.setRole(role);
+        log.info("Changed user role: {}", user);
+    }
+}
