3a65df1d36120b873a54e2b5e97dbcfc4c87c41e
Dmitriy Serov <dmitriiserov@MacBook-Air-Dmitriy.local>
2025-08-03 13:54:33 +0300

    added implement from userDetails for security

--- src/main/java/com/example/bankcards/entity/User.java
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -3,14 +3,18 @@ package com.example.bankcards.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Data
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -33,5 +37,30 @@ public class User {
         if (this.role == null) {
             this.role = Role.USER;
         }
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
