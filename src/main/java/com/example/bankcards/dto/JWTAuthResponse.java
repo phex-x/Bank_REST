1ec67721977aac38d54abee8a4ec01a4b2b0f063
Dmitriy Serov <dmitriiserov@MacBook-Air-Dmitriy.local>
2025-08-03 14:03:04 +0300

    dto for response with jwt token

---
+++ src/main/java/com/example/bankcards/dto/JWTAuthResponse.java
@@ -0,0 +1,14 @@
+package com.example.bankcards.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class JWTAuthResponse {
+    private String token;
+}
