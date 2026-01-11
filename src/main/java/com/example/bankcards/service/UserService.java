743793204f6a00ed3ed1e1fbd1d6bbca40e4ee06
dmitriiserov <dmitriiserov2005@gmail.com>
2026-01-11 18:03:07 +0300

    feat: add create and read methods fo user

---
+++ src/main/java/com/example/bankcards/service/UserService.java
@@ -0,0 +1,39 @@
+package com.example.bankcards.service;
+
+import com.example.bankcards.dto.LoginRequest;
+import com.example.bankcards.dto.RegistrationRequest;
+import com.example.bankcards.dto.UserResponse;
+import com.example.bankcards.entity.User;
+import com.example.bankcards.repository.UserRepository;
+import com.example.bankcards.util.UserMapper;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    public LoginRequest createUser(RegistrationRequest registrationRequest) {
+        if (userRepository.findByEmail(registrationRequest.getEmail()).isPresent()) {
+            throw new BadCredentialsException("user with email" + registrationRequest.getEmail() + "already exists");
+        }
+        User user = userMapper.toEntity(registrationRequest);
+        user = userRepository.save(user);
+
+        return userMapper.toLoginRequest(user);
+    }
+
+    public User getUserByEmail(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("user with email" + email + " not found"));
+
+        return user;
+    }
+}
