e866594508709b84ef051e41eb89c12f8fee0e83
dmitriiserov <dmitriiserov2005@gmail.com>
2026-01-11 22:23:28 +0300

    feat: create method dto to entity

--- src/main/java/com/example/bankcards/util/CardMapper.java
+++ src/main/java/com/example/bankcards/util/CardMapper.java
@@ -1,11 +1,18 @@
 package com.example.bankcards.util;
 
+import com.example.bankcards.dto.CardCrateRequest;
 import com.example.bankcards.dto.CardResponse;
 import com.example.bankcards.entity.Card;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CardMapper {
+    private final MaskBankCardNumber mask;
+
+    public CardMapper(MaskBankCardNumber mask) {
+        this.mask = mask;
+    }
+
     public CardResponse toCardResponse(Card card) {
         CardResponse cardResponse = new CardResponse();
         cardResponse.setId(card.getId());
@@ -15,5 +22,15 @@ public class CardMapper {
         cardResponse.setStatus(card.getStatus());
 
         return cardResponse;
+    }
+
+    public Card toCard(CardCrateRequest cardCrateRequest) {
+        Card card = new Card();
+        card.setUser(cardCrateRequest.getUser());
+        card.setNumber(cardCrateRequest.getCardNumber());
+        card.setMaskedCardNumber(mask.maskBankCardNumber(cardCrateRequest.getCardNumber()));
+        card.setExpiryDate(cardCrateRequest.getExpiryDate());
+
+        return card;
     }
 }
