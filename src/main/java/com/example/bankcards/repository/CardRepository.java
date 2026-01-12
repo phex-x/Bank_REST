5756e8e08b4f271233e2b246faec484377b1badc
dmitriiserov <dmitriiserov2005@gmail.com>
2026-01-12 14:39:47 +0300

    feat: add method find by masked number

--- src/main/java/com/example/bankcards/repository/CardRepository.java
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -1,6 +1,9 @@
 package com.example.bankcards.repository;
 
+import com.example.bankcards.dto.CardResponse;
 import com.example.bankcards.entity.Card;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,8 @@ import java.util.Optional;
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
     Optional<Card> findByNumber(String cardNumber);
+    
+    Page<Card> findAllByUserId(Long userId, Pageable pageable);
+
+    Optional<Card> findByMaskedCardNumber(String maskedCardNumber);
 }
