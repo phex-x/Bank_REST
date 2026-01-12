95221db00d22cd2f9c2917522350db6dd7d36286
dmitriiserov <dmitriiserov2005@gmail.com>
2026-01-12 14:55:43 +0300

    feat: add endpoint for transfer

--- src/main/java/com/example/bankcards/controller/UserController.java
+++ src/main/java/com/example/bankcards/controller/UserController.java
@@ -2,8 +2,11 @@ package com.example.bankcards.controller;
 
 import com.example.bankcards.dto.CardFindByNumberRequest;
 import com.example.bankcards.dto.CardResponse;
+import com.example.bankcards.dto.TransactionRequest;
+import com.example.bankcards.dto.TransactionResponse;
 import com.example.bankcards.entity.User;
 import com.example.bankcards.service.CardService;
+import com.example.bankcards.service.TransactionService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,15 +18,17 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 
 @RestController
-@RequestMapping("/api/user/card")
+@RequestMapping("/api/user")
 public class UserController {
     private final CardService cardService;
+    private final TransactionService transactionService;
 
-    public UserController(CardService cardService) {
+    public UserController(CardService cardService, TransactionService transactionService) {
         this.cardService = cardService;
+        this.transactionService = transactionService;
     }
 
-    @GetMapping("/get-all")
+    @GetMapping("/card/get-all")
     public ResponseEntity<Page<CardResponse>> getAllUserCards(Pageable pageable) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         assert auth != null;
@@ -33,7 +38,7 @@ public class UserController {
         return ResponseEntity.ok(cardService.getAllUserCards(user.getId(), pageable));
     }
 
-    @GetMapping("/find")
+    @GetMapping("/card/find")
     public ResponseEntity<Page<CardResponse>> findUserCards(
             Pageable pageable,
             @Valid @RequestBody CardFindByNumberRequest request) {
@@ -46,7 +51,7 @@ public class UserController {
         return ResponseEntity.ok(cardService.getAllUserCards(user.getId(), pageable));
     }
 
-    @GetMapping("/get-balance/{id}")
+    @GetMapping("/card/get-balance/{id}")
     public ResponseEntity<BigDecimal> getBalance(@PathVariable Long id) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         assert auth != null;
@@ -54,6 +59,16 @@ public class UserController {
         assert user != null;
 
         return ResponseEntity.ok(cardService.getBalance(user.getId(), id));
+    }
+
+    @PostMapping("/transfer")
+    public ResponseEntity<TransactionResponse> transfer(@Valid @RequestBody TransactionRequest request) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        assert auth != null;
+        User user = (User) auth.getPrincipal();
+        assert user != null;
+
+        return ResponseEntity.ok(transactionService.transfer(user.getId(), request));
     }
 
 }
